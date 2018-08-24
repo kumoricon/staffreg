f6abcb94a5e591607e3699f23e73f6e95b751d36
Jason Short <jason@sheersky.com>
2018-08-24 13:20:48 -0700

    Login work

--- staffserver/src/main/java/org/kumoricon/staffserver/login/LoginController.java
+++ staffserver/src/main/java/org/kumoricon/staffserver/login/LoginController.java
@@ -1,25 +1,27 @@
 package org.kumoricon.staffserver.login;
 
 import org.kumoricon.staffserver.exception.ForbiddenException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-@SuppressWarnings(value = "unused")
 public class LoginController {
-
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public LoginResponse login(@RequestBody LoginRequest body, HttpServletRequest request) {
-        System.out.println(request.getRemoteAddr());
+    public LoginResponse login(@RequestParam(name = "clientId") String clientId, HttpServletRequest request) {
+        log.info("clientId: {}", clientId);
+        log.info("IP: {}", request.getRemoteAddr());
+        log.info("User: {}", request.getRemoteUser());
 
+        log.info(request.getHeader("Authorization"));
         boolean loginSucceeded = true;
         if (loginSucceeded) {
-            return new LoginResponse(body.getUsername(), true);
+            return new LoginResponse(request.getRemoteUser(), true);
         } else {
             throw new ForbiddenException("Access denied");
         }
