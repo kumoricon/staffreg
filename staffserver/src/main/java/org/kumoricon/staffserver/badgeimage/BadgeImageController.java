c9eca8c1a24d343ad4e2b8a460494d4298633322
Jason Short <jason@sheersky.com>
2018-09-08 17:50:14 -0700

    Early badgeimage endpoint work

---
+++ staffserver/src/main/java/org/kumoricon/staffserver/badgeimage/BadgeImageController.java
@@ -0,0 +1,54 @@
+package org.kumoricon.staffserver.badgeimage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class BadgeImageController {
+    private static final Logger log = LoggerFactory.getLogger(BadgeImageController.class);
+
+    @Autowired
+    private BadgeImageService badgeImageService;
+
+    @GetMapping("/badgeImages")
+    public List<String> getAvailableImages(@RequestParam(value = "after", defaultValue = "0") Long after, HttpServletRequest request) {
+
+        return badgeImageService.availableFiles(after);
+    }
+
+
+//    @GetMapping("/downloadFile/{fileName:.+}")
+//    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+//        // Load file as Resource
+//        Resource resource = fileStorageService.loadFileAsResource(fileName);
+//
+//        // Try to determine file's content type
+//        String contentType = null;
+//        try {
+//            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+//        } catch (IOException ex) {
+//            logger.info("Could not determine file type.");
+//        }
+//
+//        // Fallback to the default content type if type could not be determined
+//        if(contentType == null) {
+//            contentType = "application/octet-stream";
+//        }
+//
+//        return ResponseEntity.ok()
+//                .contentType(MediaType.parseMediaType(contentType))
+//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+//                .body(resource);
+//    }
+}
---
+++ staffserver/src/main/java/org/kumoricon/staffserver/badgeimage/BadgeImageService.java
@@ -0,0 +1,76 @@
+package org.kumoricon.staffserver.badgeimage;
+
+import org.kumoricon.staffserver.imageupload.FileStorageException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BadgeImageService {
+    private static final Logger log = LoggerFactory.getLogger(BadgeImageService.class);
+    @Value("${staffreg.file.badgeimagepath}")
+    private String badgeImagePathString;
+
+    private Path badgeImagePath;
+
+//    String getFile(String filename) {
+//        String fName = StringUtils.cleanPath(filename);
+//
+//        try {
+//            if(fName.contains("..")) {
+//                throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fName);
+//            }
+//
+//            Path targetLocation = this.badgeImagePath.resolve(Instant.now().toEpochMilli() + "-" + fileName);
+//            Files.copy(file.getInputStream(), targetLocation);
+//
+//            return fileName;
+//        } catch (IOException ex) {
+//            throw new FileStorageException("Could not store file " + fileName + ". Please try again!", ex);
+//        }
+//    }
+
+    List<String> availableFiles(final Long timestampMS) {
+        if (timestampMS < 0) {
+            throw new RuntimeException("Error: timestampMS must be >= 0");
+        }
+        List<String> files = new ArrayList<>();
+
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(badgeImagePath, "*.{png,jpg}" )) {
+            for (Path path : stream) {
+                if (path.toFile().lastModified() >= timestampMS) {
+                    files.add(path.getFileName().toString());
+                }
+            }
+        } catch (IOException ex) {
+            log.error("Error getting available badge images", ex);
+        }
+
+        return files;
+
+    }
+
+    @PostConstruct
+    public void createDirectories() {
+        try {
+            badgeImagePath = Files.createDirectories(Paths.get(badgeImagePathString));
+            log.info("Badge image path: " + badgeImagePath.toAbsolutePath().toString());
+        } catch (IOException ex) {
+            log.error("Error creating directory", ex);
+        }
+    }
+
+
+}
